d3a856ca7ee647c363067cc1e1b379f1778d0f45
fwg4476 <fwg4476@naver.com>
2025-06-22 16:15:01 +0900

    Add files via upload

---
+++ Web/Backend/CapstoneApplication.java
@@ -0,0 +1,17 @@
+package com.example.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+public class CapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneApplication.class, args);
+    }
+
+}
---
+++ Web/Backend/respondTimeEntity.java
@@ -0,0 +1,35 @@
+package com.example.capstone;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class respondTimeEntity {
+    //생성 날짜
+    @CreatedDate
+    @Column(name = "create_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    //수정 날짜
+    @LastModifiedDate
+    @Column(name = "update_date")
+    private LocalDateTime modifiedDate;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.createdDate = LocalDateTime.now();
+        this.modifiedDate = this.createdDate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedDate = LocalDateTime.now();
+    }
+}
